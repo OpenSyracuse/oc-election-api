7465dcd80610895c8ec8dc15088b3c6c7202fc00
Mark Headd <mheadd@gmail.com>
2016-03-01 12:55:16 -0500

    Modified to use async

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Include required modules.
 var request = require('request');
+var async = require('async');
 var express = require('express');
 var bodyParser = require('body-parser');
 var utilities = require('./utils/parser'); 
@@ -15,6 +16,61 @@ var URL = 'http://vic.ntsdata.com/onondagaboe/pollingplacelookup.aspx';
 // Address parts for polling location lookup.
 var house_num, street_name, apartment, zip;
 
+// Serve static web assets.
+app.use(express.static(__dirname + '/public'));
+
+// Route for polling location lookup.
+app.post('/', function(req, res) {
+	async.waterfall([ 
+		async.apply(getFormData, req.body), 
+		getPollingInfo 
+		],
+		// Process response and send to user.
+		function (error, body) {
+			if(!error) {
+				var pollingLocation = utilities.getLocationInfo(body);
+				res.json(pollingLocation);
+			}
+			else {
+				var error_message = error.message.length >0 ? error.message  : "Unable to retrieve polling location information.";
+				var error_code = error.code > 0 ? error.code : 500;
+				res.status(error_code).json({error: error_message});
+			}
+	});
+});
+
+// Get the viewstate & related bits for polling place lookup.
+function getFormData(body, callback) {
+	validateAddressParts(body);
+	if (house_num && street_name && zip) {
+		request(URL, function (error, response, body) {
+			callback(error, body);
+		});
+	}
+	else {
+		callback({message: "Invalid request. Must POST house_num, street_name and zip.", code: 400});
+	}
+}
+
+// Assemble form data to be sent with subsequent request.
+function getPollingInfo(body, callback) {
+	
+	var viewStateData = utilities.getViewState(body);
+	var formData = {
+		__VIEWSTATE: viewStateData.viewState,
+		__VIEWSTATEGENERATOR: viewStateData.viewStateGenerator,
+		__EVENTVALIDATION: viewStateData.eventValidation,
+		czip: zip,
+		cstreet_no: house_num,
+		cstreet_nm: street_name,
+		capartment: apartment,
+		cmdlookup: 'Lookup'
+	};
+	request.post({url:URL, form: formData}, function (error, response, body) {
+		callback(error, body);
+	});
+}
+
 // Validation of required address parts.
 function validateAddressParts(body) {
 	house_num = body.house_num;
@@ -22,54 +78,3 @@ function validateAddressParts(body) {
 	apartment = body.apartment;
 	zip = body.zip;
 }
-
-// Serve static web assets.
-app.use(express.static(__dirname + '/public'));
-
-app.post('/', function(req, res) {
-	validateAddressParts(req.body);
-	if (house_num && street_name && zip) {
-		request(URL, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-
-				// Get the viewstate & related bits for polling place lookup.
-				var viewStateData = utilities.getViewState(body);
-
-				// Assemble form data to be sent with subsequent request.
-				var formData = {
-					__VIEWSTATE: viewStateData.viewState,
-					__VIEWSTATEGENERATOR: viewStateData.viewStateGenerator,
-					__EVENTVALIDATION: viewStateData.eventValidation,
-					czip: zip,
-					cstreet_no: house_num,
-					cstreet_nm: street_name,
-					capartment: apartment,
-					cmdlookup: 'Lookup'
-				};
-
-				// POST form data to do location lookup.
-				request.post({url:URL, form: formData}, function (error, response, body) {
-				  if (!error && response.statusCode == 200) {
-
-				    // Construct JSON object with polling location information.
-				    var pollingLocation = utilities.getLocationInfo(body);
-
-				    // Send response.
-				    res.set('Content-type', 'application/json');
-				    res.send(pollingLocation);
-				  }
-				  else {
-				  	res.status(500).send("Unable to retrieve polling location information.");
-				  }
-				});
-				
-			}
-			else {
-				res.status(500).send("Unable to access County polling location website.");
-			}
-		});
-	}
-	else {
-		res.status(400).send("Invalid request. Must POST house_num, street_name and zip.");
-	}
-});
